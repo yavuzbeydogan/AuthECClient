d008faa492e1b09b6d69cf0933f3dc51f1b89b93
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-17 15:16:03 +0300

    initial commit

---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,20 @@
+import { FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.component.html',
+  styles: ``
+})
+export class RegistrationComponent {
+  constructor(public formBuilder: FormBuilder) {}
+    form = this.formBuilder.group({
+    fullName: [''],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+  });
+}
