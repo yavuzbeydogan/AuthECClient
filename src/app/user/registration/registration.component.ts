f08da6718f53a260baf3d77fd547057aaea74efc
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-18 15:43:51 +0300

    register işlemi.

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,6 +1,7 @@
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -11,12 +12,23 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class RegistrationComponent {
   constructor(public formBuilder: FormBuilder) {}
+
+    passwordMatchValidator:ValidatorFn=(control:AbstractControl):null => {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+      if (password && confirmPassword && password.value !== confirmPassword.value) 
+        confirmPassword?.setErrors({ passwordMismatch: true })
+      else 
+      confirmPassword?.setErrors(null)
+      return null;
+    }
     form = this.formBuilder.group({
-    fullName: [''],
-    email: [''],
-    password: [''],
+    fullName: ['',Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['',[Validators.required,Validators.minLength(6),Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$')]],
     confirmPassword: [''],
-  });
+  }, {Validators: this.passwordMatchValidator}
+);
   onSubmit() {
   console.log(this.form.value);}
 }
