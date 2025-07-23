ac72456a8a71c7b0fa895221d88c7360c866b773
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-23 10:05:24 +0300

    Login Formlar oluşturuldu.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
-
+  constructor(public FormBuilder:FormBuilder) {}
+  form=this.FormBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+})
 }
