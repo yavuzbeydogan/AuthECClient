a68a3936af512ededafad78b4867edf7e307aa12
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-24 14:01:47 +0300

    ana dizin belirlendi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './user/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+    {path:'', redirectTo:'/signin', pathMatch:'full'},
     {path:'', component:UserComponent,
         children: [
             {path:'signup', component:RegistrationComponent},
