3f7c4fb16d23e0e1c78e0060d3cc277c2fc995a0
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-24 15:25:56 +0300

    logout işlemi yapıldı.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}
 
+  onLogout(){
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/signin');
+  }
 }
