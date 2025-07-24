0eaea83afb55176f4105d7cdfe54bb7b3b298c2a
Yavuz Alp Beydogan <157284884+yavuzbeydogan@users.noreply.github.com>
2025-07-24 13:59:44 +0300

    animasyon eklendi

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { RegistrationComponent } from './registration/registration.component';
-import { RouterOutlet } from "@angular/router";
-
+import { ChildrenOutletContexts, RouterOutlet } from "@angular/router";
+import {trigger, style, animate, transition, query} from "@angular/animations"; 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [RegistrationComponent, RouterOutlet],
   templateUrl: './user.component.html',
-  styles: ``
+  styles: ``,
+  animations: [
+    trigger('routerFadeIn',[
+      transition('* <=> *',[
+        query(':enter',[
+          style({opacity:0}),
+          animate('1s ease-in-out', style({opacity:1}))
+        ],{optional:true})
+      ])
+    ])
+  ]
 })
-export class UserComponent {
 
+export class UserComponent {
+  constructor(private context: ChildrenOutletContexts){}
+  getRouteUrl() {
+    return this.context.getContext('primary')?.route?.url;
+
+}
 }
